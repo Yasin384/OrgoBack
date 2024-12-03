4e6c12b6abfc50e77412228434877baa0c4df31c
yasin <asinarstanbekov51@gmail.com>
2024-12-03 13:27:39 +0600

    deploying

--- Orgo_Back/settings.py
+++ Orgo_Back/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
     
 ]
 CORS_ALLOW_CREDENTIALS = True
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Application definition
 
 INSTALLED_APPS = [
