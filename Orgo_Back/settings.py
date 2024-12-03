1fa46f11975adb5dfc8d3aa62e2549771f716f42
yasin <asinarstanbekov51@gmail.com>
2024-12-03 12:48:59 +0600

    deploying

--- Orgo_Back/settings.py
+++ Orgo_Back/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7gx)4y4h2k1)0&&k(aaoo!%bukb5j&%25$iwhz$rjk6jtislfw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
