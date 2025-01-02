5ee28f71f48c70bada62747635a35df7561500bc
Yasin384 <72139082+Yasin384@users.noreply.github.com>
2025-01-02 08:18:36 +0600

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,6 +1,6 @@
 # main/models.py
 
-  from django.db import models
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.utils import timezone
